53106b7b70bc2e17a0cc43ba4c807482269342f9
Mayank Tanwa <mayank.tanwar@sjsu.edu>
2017-05-01 02:58:59 -0700

    final submission

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 //
 var sprout = angular.module('sprout',['ui.bootstrap']);
 
-sprout.controller('landingPage', function($scope){
+sprout.controller('landingPage', function($scope,$sce){
 
     $scope.myInterval = 5000;
     $scope.noWrapSlides = false;
@@ -12,25 +12,35 @@ sprout.controller('landingPage', function($scope){
 
     $scope.images =[{
                     "type":"image",
-                     "src":"../images/image1.jpg"
+                     "src":"../images/bigdata1.jpg"
                     },
                     {
                         "type":"image",
-                        "src":"../images/image2.jpg"
+                        "src":"../images/iot1.jpg"
                     } ,
                     {
                         "type":"image",
-                        "src":"../images/image4.jpg"
+                        "src":"../images/cloud1.jpg"
                     },
                     {
                         "type":"video",
-                        "src":"https://www.youtube.com/watch?v=7D1CQ_LOizA"
+                        "src":"https://www.youtube.com/embed/QSIPNhOiMoE"
                     }
                     ]
 
-    $scope.products=[{"title":"Big Data","data":"XYX", "src":"../images/image1.jpg"},
-        {"title":"Big Data","data":"XYX", "src":"../images/image1.jpg"},
-        {"title":"Big Data","data":"XYX", "src":"../images/image1.jpg"}];
+    $scope.products=[{"title":"Cloud Computing","data":"Yes, everything we do is hosted on cloud!", "src":"../images/cloud.jpg"},
+        {"title":"Internet of Things","data":"We are the data crunchers!", "src":"../images/iot.jpg"},
+        {"title":"Big Data","data":"Oh, we meant, Internet of Everything!", "src":"../images/bigdata.jpg"}];
+
+    $scope.tweets =[{"name":"@Iot","data":"It's Official: The Internet Of Things Takes Over Big Data As The Most Hyped Technology. - Forbes"}
+    ,{"name":"@bigData","data":"In God we all trust, all others must bring data. - W. Edwards Deming"},
+        {"name":"@cloud","data":"I don’t need a hard disk in my computer if I can get to the server faster… carrying around these non-connected computers is byzantine by comparison.” – Steve Jobs"}]
+
+    $scope.trust = function(url){
+
+        return $sce.trustAsResourceUrl(url);
+    }
+
 
 });
 
@@ -38,4 +48,8 @@ sprout.filter("trustUrl", ['$sce', function ($sce) {
     return function (recordingUrl) {
         return $sce.trustAsResourceUrl(recordingUrl);
     };
-}]);
+}
+
+]);
+
+
